9baa0ce9831393d6c1e10f0fc1e625027a35c775
nvianin <31324783+nvianin@users.noreply.github.com>
2020-05-11 11:36:50 +0200

    Update main.js
    circles & distance fields

--- cahier/nathanael/ex_1/main.js
+++ cahier/nathanael/ex_1/main.js
@@ -17,12 +17,43 @@ function setup() {
 
 function draw() {
   background(255);
+
+  const x1 = Math.sin(frameCount * 0.01) * .5;
+  const y1 = Math.sin(frameCount * 0.04) * 0.2;
+  const x2 = Math.sin(frameCount * 0.02) * .235;
+  const y2 = Math.sin(frameCount * 0.03) * .5;
+
   for (var y = 0; y < NUM_Y; y++) {
     for (var x = 0; x < NUM_X; x++) {
       var i = x * NUM_Y + y;
-      data[i] = (x + y) % 2;
+      const u = (x * 2 - NUM_X) / NUM_X;
+      const v = (y * 2 - NUM_Y) / NUM_Y;
+      let d = 1e10;
+      d = Math.min(dist(0 + x1, 0 + y1, u, v) - 0.2, d);
+      d = Math.min(dist(0.5 + x2, 0 + y2, u, v) - 0.3, d);
+      var margin = 0.8;
+
+      /* if (d < margin + 0.07 && d > margin) {
+        data[i] = 1;
+      } */
+
+      data[i] = Math.sign(0.8 - Math.exp(-5 * Math.abs(d)));
+      /* data[i] = d; */
     }
   }
+  /*   const radius = 16;
+  for (var t = 0; t < 360; t++) {
+    var x = Math.sin(t) * radius;
+    var y = Math.cos(t) * radius;
+    x += NUM_X / 2;
+    y += NUM_Y / 2;
+    x = Math.floor(x);
+    y = Math.floor(y);
+    var i = x * NUM_Y + y;
+    data[i] = 1;
+  } */
+  textSize(8);
+  textAlign(CENTER, CENTER);
 
   for (var y = 0; y < NUM_Y; y++) {
     for (var x = 0; x < NUM_X; x++) {
@@ -32,6 +63,9 @@ function draw() {
       const v = data[i];
       fill(v * 255);
       rect(posX, posY, CELL, CELL);
+
+      /* fill(255, 125, 0);
+      text(v, x * CELL + xMargin, y * CELL + yMargin); */
     }
   }
 }
