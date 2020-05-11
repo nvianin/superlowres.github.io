2245bfa8c360c39dab1fce5dd5370f21b5ccfeeb
Andreas <andreas@ertdfgcvb.com>
2020-05-11 10:42:11 +0200

    Center render output

--- cahier/andreas/ex_1/main.js
+++ cahier/andreas/ex_1/main.js
@@ -6,9 +6,7 @@ const NUM_X = 32
 const NUM_Y = 32
 const CELL  = 16
 
-
 const data = new Array(NUM_X * NUM_Y)
-
 
 function setup(){
 	createCanvas(windowWidth, windowHeight)
@@ -24,17 +22,20 @@ function draw(){
 	}
 
 	// rendering
+
+	const ox = (width - NUM_X * CELL) / 2
+	const oy = (height - NUM_Y * CELL) / 2
+
 	for(let j=0; j<NUM_Y; j++) {
 		for(let i=0; i<NUM_X; i++) {
-			const x = i * CELL + 20
-			const y = j * CELL + 20
+			const x = i * CELL + ox
+			const y = j * CELL + oy
 			const idx = i + j * NUM_X
 			const v = data[idx]
 			fill(v * 255)
 			rect(x, y, CELL, CELL)
 		}
 	}
-
 }
 
 function windowResized(){
